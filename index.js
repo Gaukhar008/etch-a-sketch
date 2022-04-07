da0c8cdf7f22f7c782011c8d3648619e3ce27381
Gaukhar <gauhar.abdizhapbarova@gmail.com>
2022-04-07 23:33:57 +0600

    Add reset button, change to positions

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const container = document.getElementById('container');
+const header = document.createElement('div');
+const mainContent = document.createElement('div');
+const buttons = document.createElement('div');
+const resetButton = document.createElement('button');
 const grid = document.createElement('div');
+
+mainContent.classList.add('content')
+header.classList.add('header');
+header.textContent = 'Etch-a-Sketch';
+header.style.cssText = 'color: white; font-size: 45px'
+buttons.classList.add('buttons');
+resetButton.classList.add('resetButton');
+resetButton.textContent = 'Reset';
 grid.classList.add('grid');
-container.appendChild(grid);
+
+buttons.appendChild(resetButton);
+mainContent.appendChild(header);
+mainContent.appendChild(grid);
+container.appendChild(buttons);
+container.appendChild(mainContent);
 
 function createGrid(rows, columns) {
     container.style.setProperty('--grid-rows', rows);
@@ -9,9 +26,8 @@ function createGrid(rows, columns) {
     for(let i = 0; i < rows*columns; i++) {
         let cell = document.createElement('div');
         grid.appendChild(cell).className = "grid-item";
-        cell.addEventListener('mouseover', e => 
-        e.target.classList.add('my-color-class'));
     }
+    document.querySelectorAll('.grid-item').forEach(cell => cell.addEventListener('mouseover', (e) => { 
+    e.target.style.backgroundColor = 'black'}))
 }
-
 createGrid(16, 16);
